c0c71110a974338d9e3df9f36fe148e0f79750eb
Dlama istifanus <dlamaistifanus0@gmail.com>
2022-04-29 23:12:47 +0100

    new commit

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-var logo = document.getElementsByClassName('fa-dyalog')
-var headContainer = document.getElementById('header-container');
-var header = document.getElementById('header');
-var links = document.getElementById('links');
-var cancel = document.getElementsByClassName('fa-times')
+let logo = document.getElementsByClassName('fa-dyalog')
+let headContainer = document.getElementById('header-container');
+let header = document.getElementById('header');
+let links = document.getElementById('links');
+let cancel = document.getElementsByClassName('fa-times');
+let firstSec = document.getElementById('first-section')
+
+window.addEventListener('scroll', function(e){
+    if(window.scrollY > 10){
+        header.style.boxShadow = '0px 15px 20px -18px rgba(0,0,0,0.75)'
+        header.style.position = 'fixed';
+        firstSec.style.paddingTop = 0;
+    }else{
+        header.style.boxShadow = 'none';
+        
+    }
+})
+
+
+
 
 logo[0].addEventListener('click', function(){
     links.style.display = 'block';
