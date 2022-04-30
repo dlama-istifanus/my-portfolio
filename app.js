6261125d4b78cfa7d59bdbd5bb3a2cb3aed7b4dd
Dlama istifanus <dlamaistifanus0@gmail.com>
2022-04-30 21:50:51 +0100

    commit

--- app.js
+++ app.js
@@ -4,24 +4,14 @@ let header = document.getElementById('header');
 let links = document.getElementById('links');
 let firstSec = document.getElementById('first-section')
 
-// window.addEventListener('scroll', function(e){
-//     if(window.scrollY > 10){
-//         header.style.boxShadow = '0px 15px 20px -18px rgba(0,0,0,0.75)'
-//         header.style.position = 'fixed';
-//         firstSec.style.paddingTop = 0;
-//     }else{
-//         header.style.boxShadow = 'none';
+window.addEventListener('scroll', function(e){
+    if(window.scrollY > 10){
+        header.style.boxShadow = '0px 15px 20px -18px rgba(0,0,0,0.75)'
+        header.style.position = 'fixed';
+        firstSec.style.paddingTop = 0;
+    }else{
+        header.style.boxShadow = 'none';
         
-//     }
-// })
-
-
-
-
-logo[0].addEventListener('click', function(){
-    links.style.display = 'block';
-    headContainer.style.backgroundColor = '#ffff';
-    logo[0].style.color = '#7149BC';
-    header.style.marginLeft = '30px'
-    cancel[0].style.display = 'block'
+    }
 })
+
