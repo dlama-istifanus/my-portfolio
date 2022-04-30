fe724384dd4616844045735fe64c9b69c0ac4fa2
Dlama istifanus <dlamaistifanus0@gmail.com>
2022-04-30 11:28:10 +0100

    commit

--- app.js
+++ app.js
@@ -2,19 +2,18 @@ let logo = document.getElementsByClassName('fa-dyalog')
 let headContainer = document.getElementById('header-container');
 let header = document.getElementById('header');
 let links = document.getElementById('links');
-let cancel = document.getElementsByClassName('fa-times');
 let firstSec = document.getElementById('first-section')
 
-window.addEventListener('scroll', function(e){
-    if(window.scrollY > 10){
-        header.style.boxShadow = '0px 15px 20px -18px rgba(0,0,0,0.75)'
-        header.style.position = 'fixed';
-        firstSec.style.paddingTop = 0;
-    }else{
-        header.style.boxShadow = 'none';
+// window.addEventListener('scroll', function(e){
+//     if(window.scrollY > 10){
+//         header.style.boxShadow = '0px 15px 20px -18px rgba(0,0,0,0.75)'
+//         header.style.position = 'fixed';
+//         firstSec.style.paddingTop = 0;
+//     }else{
+//         header.style.boxShadow = 'none';
         
-    }
-})
+//     }
+// })
 
 
 
@@ -25,11 +24,4 @@ logo[0].addEventListener('click', function(){
     logo[0].style.color = '#7149BC';
     header.style.marginLeft = '30px'
     cancel[0].style.display = 'block'
-})
-
-cancel[0].addEventListener('click', function(){
-    links.style.display = 'none';
-    headContainer.style.backgroundColor = '#7149BC';
-    logo[0].style.color = '#ffff';
-    header.style.marginLeft = '30px'
 })
